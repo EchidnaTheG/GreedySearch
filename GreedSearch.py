62b36c5919c3a238588750c1f175e41b2eecec82
Echidna <ej.cruz.sant@gmail.com>
2025-02-16 01:19:26 -0400

    Did Some changes, overall hard to say any difference apart from the addition of a simple way to break the while loop

--- GreedSearch.py
+++ GreedSearch.py
@@ -2,11 +2,16 @@ import os
 from searchlogic import *
 def main():
     print("INITIALIAZING FILE EXPLORER")
+    print("COMMANDS!\ncd -> change into a dir\n.. -> change into previous dir\nexit() -> Exit Program")
     cwd= os.getcwd()
-    show(cwd)
     print(f"INIT AT {cwd}")
-    while True:
+    show(cwd)
+    program_on= True
+    while program_on:
         command=input("TYPE A COMMAND: ")
+        if command =="exit()":
+            program_on= False
+            break
         cwd=navegation(command, cwd)
         show(cwd)
   
--- searchlogic.py
+++ searchlogic.py
@@ -4,13 +4,16 @@ def show(dir):
     print(list_of_dirs)
     return list_of_dirs
 def navegation(command,dir):
-    if command == "..":
-        new_dir= "/".join(dir.split("/")[:-1]) or "/"
-        os.chdir(new_dir)
-        print(f"Now at{new_dir}")
-        return new_dir
-    if command == "cd":
-        path=input("WHERE TO?: ")
-        os.chdir(path)
-        print(f"Now at{path}")
-        return path
+    try:
+        if command == "..":
+            new_dir= "/".join(dir.split("/")[:-1]) or "/"
+            os.chdir(new_dir)
+            print(f"Now at{new_dir}")
+            return new_dir
+        if command == "cd":
+            path=input("WHERE TO?: ")
+            os.chdir(path)
+            print(f"Now at{path}")
+            return path
+    except:
+        print("ERROR")
